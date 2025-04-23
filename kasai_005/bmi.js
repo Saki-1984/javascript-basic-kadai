eb8a167163ea6f517f51588a78eb2f800056ec68
Saki-1984 <sjar.0w0jtpmgpn@gmail.com>
2025-04-23 23:00:08 +0900

    bmi.jsの再アップロード

--- kasai_005/bmi.js
+++ kasai_005/bmi.js
@@ -1,10 +1,12 @@
 //変数を宣言
 let heightNumber;
 let wheightNumber;
+let bmi;
 
 //値の代入
 heightNumber = '1.7';
 wheightNumber = '68';
+bmi = wheightNumber / (heightNumber * heightNumber);
 
 //コンソールへの出力
-console.log(wheightNumber / (heightNumber * heightNumber));
+console.log(bmi);
